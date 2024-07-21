46c1d89029e77b7e0e491645b50a4a8ca3d71741
Patrick Rose <patrick.rose@theskillsnetwork.com>
2024-07-21 15:16:18 +0100

    Initial commit for running hot bot

---
+++ src/commands/index.ts
@@ -0,0 +1,7 @@
+import * as refresh from "./refresh";
+import * as play from "./play";
+
+export const commands = {
+    refresh,
+    play
+};
---
+++ src/commands/refresh.ts
@@ -0,0 +1,33 @@
+import {CommandInteraction, REST, Routes, SlashCommandBuilder} from "discord.js";
+import {config} from "../config";
+import {commands} from "./index";
+
+export const data = new SlashCommandBuilder()
+    .setName("refresh")
+    .setDescription("Refreshes commands");
+
+export async function execute(interaction: CommandInteraction) {
+    if (!interaction.guildId) {
+        return interaction.reply("Unable to refresh");
+    }
+
+    const guildId = interaction.guildId;
+
+    const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
+
+    const commandsData = Object.values(commands).map((command) => command.data);
+
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands(config.DISCORD_CLIENT_ID, guildId),
+            {
+                body: commandsData,
+            }
+        );
+
+        return interaction.reply("Successfully reloaded application commands.");
+    } catch (error) {
+        console.error(error);
+        return interaction.reply("Unable to refresh");
+    }
+}
